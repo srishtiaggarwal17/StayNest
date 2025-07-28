39a54e1f41074312894249e9bfb838e3cb2f1dd1
srishtiaggarwal17 <159170123+srishtiaggarwal17@users.noreply.github.com>
2025-07-28 20:05:31 +0530

    Update PaymentPage.jsx

--- frontend/src/pages/PaymentPage.jsx
+++ frontend/src/pages/PaymentPage.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import axios from "axios";
 import {BOOKING_API_END_POINT, STRIPE_PUBLISHABLE_KEY } from "@/utils/constant";
+import { toast } from "sonner";
 
-// Load Stripe with your public key (from .env)
 const stripePromise = loadStripe(STRIPE_PUBLISHABLE_KEY);
 
 const Payment = ({ room, user, checkInDate, checkOutDate, guests, totalPrice }) => {
@@ -12,19 +12,22 @@ const Payment = ({ room, user, checkInDate, checkOutDate, guests, totalPrice }) 
       const stripe = await stripePromise;
 
       const response = await axios.post(`${BOOKING_API_END_POINT}/payment`, {
-        room:room.hotel.name,
-        user,
+        roomId:room._id,
+        userId:user._id,
         checkInDate,
         checkOutDate,
         guests,
         totalPrice,
       },{withCredentials:true});
-
+      if (!response.data?.url) {
+        toast.error("Failed to initiate payment."); // or alert
+        return;
+      }
       // Redirect to Stripe Checkout
       window.location.href = response.data.url;
     } catch (error) {
       console.error("Payment error:", error);
-      alert("Payment failed. Please try again.");
+      toast.error("Payment failed. Please try again.");
     }
   };
 
