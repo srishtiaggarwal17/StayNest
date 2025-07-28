fd3cfed314d5a38a6f27489dc4e505379fd33037
srishtiaggarwal17 <159170123+srishtiaggarwal17@users.noreply.github.com>
2025-07-28 19:35:43 +0530

    Update PaymentPage.jsx

--- frontend/src/pages/PaymentPage.jsx
+++ frontend/src/pages/PaymentPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import axios from "axios";
-import { STRIPE_PUBLISHABLE_KEY } from "@/utils/constant";
+import {BOOKING_API_END_POINT, STRIPE_PUBLISHABLE_KEY } from "@/utils/constant";
 
 // Load Stripe with your public key (from .env)
 const stripePromise = loadStripe(STRIPE_PUBLISHABLE_KEY);
@@ -11,7 +11,7 @@ const Payment = ({ room, user, checkInDate, checkOutDate, guests, totalPrice }) 
     try {
       const stripe = await stripePromise;
 
-      const response = await axios.post("http://localhost:4000/api/v1/booking/payment", {
+      const response = await axios.post(`${BOOKING_API_END_POINT}/payment`, {
         room:room.hotel.name,
         user,
         checkInDate,
