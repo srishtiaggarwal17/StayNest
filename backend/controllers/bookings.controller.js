d2269c00ffaa20ff2b6aedfa2d7da9897cc424db
srishtiaggarwal17 <159170123+srishtiaggarwal17@users.noreply.github.com>
2025-07-28 17:02:29 +0530

    Update bookings.controller.js

--- backend/controllers/bookings.controller.js
+++ backend/controllers/bookings.controller.js
@@ -188,13 +188,6 @@ export const createCheckoutSession=async(req,res)=>{
   }
 }
 
-
-
-
-
-
-
-
 export const subscribeNewsletter = async (req, res) => {
   const { email } = req.body;
   if (!email) return res.status(400).json({ message: "Email is required" });
