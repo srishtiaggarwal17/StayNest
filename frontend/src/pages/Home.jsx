be019f5d43789caa451af40d1654692a0ddad351
srishtiaggarwal17 <159170123+srishtiaggarwal17@users.noreply.github.com>
2025-07-29 22:25:26 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,11 +3,13 @@ import HeroSection from "@/components/HeroSection";
 import ExclusiveOffers from "@/components/ExclusiveOffers";
 import LatestHotels from "@/components/LatestHotels";
 import Newsletter from "@/components/NewsLetter";
+import TrendingDestinations from "@/components/TrendingDestinations";
 
 const Home=()=>{
     return(
         <div>
             <HeroSection/>
+            <TrendingDestinations/>
             <LatestHotels/>
             {/* <ExclusiveOffers/> */}
             <Newsletter/>
