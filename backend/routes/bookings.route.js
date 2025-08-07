5147dbc0b493a4902f50da494a4e26351f9f7e0c
srishtiaggarwal17 <159170123+srishtiaggarwal17@users.noreply.github.com>
2025-08-07 18:16:18 +0530

    Update bookings.route.js

--- backend/routes/bookings.route.js
+++ backend/routes/bookings.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { checkAvailabilityApi, createBooking, createCheckoutSession, getHotelBookings, getUserBookings, subscribeNewsletter} from "../controllers/bookings.controller.js";
+import { checkAvailabilityApi, checkMultipleAvailability, createBooking, createCheckoutSession, getHotelBookings, getUserBookings, subscribeNewsletter} from "../controllers/bookings.controller.js";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
 
 const router=express.Router()
 
 router.post('/check-availability',checkAvailabilityApi)
+router.post("/check-multiple-availability",checkMultipleAvailability);
 router.post('/book',isAuthenticated,createBooking)
 router.get('/user',isAuthenticated,getUserBookings)
 router.get('/hotel',isAuthenticated,getHotelBookings)
