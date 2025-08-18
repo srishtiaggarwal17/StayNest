3fdac0cc0fe8a6befa4a132e5015c7faf1825565
srishtiaggarwal17 <159170123+srishtiaggarwal17@users.noreply.github.com>
2025-08-18 14:44:05 +0530

    Update room.route.js

--- backend/routes/room.route.js
+++ backend/routes/room.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {createRoom,getAllRooms,toggleRoomAvailability,getOwnerRooms, getAvailableRooms} from "../controllers/room.controller.js";
+import {createRoom,getAllRooms,toggleRoomAvailability,getOwnerRooms, getAvailableRooms, getRooms} from "../controllers/room.controller.js";
 import upload from "../middlewares/multer.js";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
 
@@ -7,6 +7,7 @@ const router = express.Router();
 
 router.post("/", upload.array("images",4),isAuthenticated,createRoom);
 router.get("/getRooms", getAllRooms);
+router.get("/getR", getRooms);
 router.get("/owner", isAuthenticated,getOwnerRooms);
 router.get('/toggle-availability',isAuthenticated,toggleRoomAvailability)
 router.get("/availableRooms", getAvailableRooms); 
